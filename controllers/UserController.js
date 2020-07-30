315973b24683529521aaf5a84b821f7c59d690d4
Francesc Contreras <francesc.contreras@udl.cat>
2020-07-30 17:13:40 +0200

    Create user: DONE

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 /* Imports */
 const User = require('../schemas/user');
+const bcrypt = require('bcrypt');
 
 /*User Controllers*/
 
@@ -10,13 +11,33 @@ exports.getUsers = async (req,res,next) => {
 
 exports.getUser = async (req,res,next) => {
     const user = await User.findOne({email:req.params.id},{password:0,__v:0})
-    if(user !== null){
-        res.status(200).send({user:user});
-    }else {
-        res.status(404).send({message:"User not found with email: " + req.params.id});
-    }
+    user !== null ? res.status(200).send({user:user}) : res.status(404).send({message:"User not found with email: " + req.params.id});
 };
 
+
 exports.createUser = async (req,res,next) => {
+
+    const user = new User({first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, 10),
+        roles:req.body.roles});
+
+    user.save().then(()=>{
+        res.status(201).send({message:"User created."})
+    }).catch((error)=>{
+        res.status(400).send({error:error});
+    });
+}
+
+exports.editUser = async (req,res,next) => {
+
+}
+
+exports.deleteUser = async (req,res,next) => {
+
+}
+
+exports.resetPassword = async (req,res,next) => {
 
 }
--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,10 @@ const UserController = require('../controllers/UserController');
 const {authorize} = require('../utils/token');
 
 router.get('/', authorize, UserController.getUsers);
+router.post('/', authorize, UserController.createUser);
 router.get('/:id', authorize, UserController.getUser);
-/*router.post('/:id', authorize, UserController.getUser);
-router.patch('/:id', authorize, UserController.getUser);
-router.delete('/:id', authorize, UserController.getUser);*/
+router.patch('/:id', authorize, UserController.editUser);
+router.delete('/:id', authorize, UserController.deleteUser);
+router.post('/reset/password', authorize, UserController.resetPassword);
 
 module.exports = router;
